0e103b69999bc9ace76d8366cb2fde14d35e342c
pantxisto <jonandonicastelo@outlook.com>
2022-10-04 22:54:27 +0100

    HttpClientModule added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
 })
-export class SharedModule { }
+export class SharedModule {}
